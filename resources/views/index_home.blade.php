7add84cc44ca55b36d7a645a7f290d7cc2d17fe2
Simran <kazmisimran@gmail.com>
2020-05-03 14:04:59 +0200

    Home-intégration+sass

---
+++ resources/views/index_home.blade.php
@@ -0,0 +1,32 @@
+@extends('layout.app_home')
+
+@section('content')
+
+    @include('templates.home_header')
+
+    @include('templates.home_intro')
+
+    @include('templates.home_about')
+
+    @include('templates.home_testimonial')
+
+    @include('templates.home_services')
+    
+    @include('templates.home_team')
+    
+    @include('templates.home_promotion')
+
+    @include('templates.home_contact')
+
+    @include('templates.home_footer')
+
+@endsection
+
+
+
+
+
+
+
+
+
